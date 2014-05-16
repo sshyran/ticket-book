e940ec27027a61d527397348fec517a421acd7bf
duongnamphuong <duongnamphuong@yahoo.com.vn>
2014-05-16 18:41:24 +0000

    bổ sung driver String:     public static String getDriverString() {         return "com.microsoft.sqlserver.jdbc.SQLServerDriver";     }

--- ticket-book/src/java/DAL/DAO.java
+++ ticket-book/src/java/DAL/DAO.java
@@ -8,11 +8,16 @@ package DAL;
  *
  * @author khoatnd
  */
-
 public class DAO {
-    
+
     /// Lay Chuoi Ket Noi CSDL
     public static String getConnectionString() {
-        return "jdbc:sqlserver://localhost;user=sa;password=123;database=TripBooking";
+        //return "jdbc:sqlserver://localhost;user=sa;password=123456;database=TripBooking";
+        return "jdbc:sqlserver://PHUONGDNSE61089\\SQLEXPRESS:53968;databaseName = TripBooking;user = sa; password = 123456";
+    }
+
+    //Lấy chuỗi driver
+    public static String getDriverString() {
+        return "com.microsoft.sqlserver.jdbc.SQLServerDriver";
     }
 }
