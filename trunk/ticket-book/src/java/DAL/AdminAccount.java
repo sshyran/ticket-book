94d0cf9b8f20120cd1a3b70b0411448fdf6295c7
ThanhDXSE60835@fpt.edu.vn <ThanhDXSE60835@fpt.edu.vn@61fedcab-b449-ba23-355b-cf856aa6f753>
2014-05-19 06:31:57 +0000

    [ThanhDX] Add object

--- trunk/ticket-book/src/java/DAL/AdminAccount.java
+++ trunk/ticket-book/src/java/DAL/AdminAccount.java
@@ -11,5 +11,41 @@ import java.io.Serializable;
  * @author Jessie
  */
 public class AdminAccount implements Serializable{
-    
+    private String username;
+    private String password;
+    private String fullName;
+
+    public AdminAccount() {
+    }
+
+    public AdminAccount(String username, String password, String fullName) {
+        this.username = username;
+        this.password = password;
+        this.fullName = fullName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
 }
